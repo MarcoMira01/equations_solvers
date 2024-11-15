dc12ac82934b349d3f964677623c68423750fd7f
Marco <marco.mirabilio@gmail.com>
2024-11-15 17:14:57 +0100

    Implementation of automatic step size control

--- my_solvers/solvers/Solver_Han.py
+++ my_solvers/solvers/Solver_Han.py
@@ -5,9 +5,11 @@ from my_solvers.core.Solver import Solver
 from typing                 import Callable
 from numpy.linalg           import norm
 from numpy                  import isscalar
-from multipledispatch       import dispatch
-from typing                 import overload
-from multimethods import multimethod
+from math                   import log
+
+# from multipledispatch       import dispatch
+# from typing                 import overload
+# from multimethods import multimethod
 
 class Solver_Han( Solver ):
 
@@ -47,11 +49,18 @@ class Solver_Han( Solver ):
     # ====================== #
     # Solver implementation
     # ====================== #
-    def solve( self , fcn_hndl: Callable , X0: float ):
+    def solve( self , fcn_hndl: Callable , X0: float , step_max: float = 1e3 ):
         if not self.__step_adpt:
             X = self.__solver_fixed_step( fcn_hndl , X0 )
-            return X
-        
+        else:
+            X = self.__solver_var_step( fcn_hndl , X0 , step_max )
+        return X
+    
+    # Implementation of Han solver in the case of fixed step
+    # - If the step is a scalar, then we follow the base approach of
+    #   iterating three times the process by doubling each time the
+    #   step value
+    # - Otherwise, if it is a vector, we iterate over it
     def __solver_fixed_step( self , fcn_hndl: Callable , X0: float ):
 
         # 0) Define local parameters for easy of reading
@@ -92,6 +101,67 @@ class Solver_Han( Solver ):
 
             h[i] = h[i]*2
             w = h[i]/( h[i] + e )
+
+            if ( n >= self._max_iter ):
+                print("Solver stopped since max number of iterations reached")
+
+        return Xn1_0
+    
+    # Implementation of Han solver with automatic step size control
+    # - The step set at the beginning is used as starting value
+    # - If the step was set as a vector, then only the first value is considered
+    def __solver_var_step( self , fcn_hndl: Callable , X0: float , step_max: float ):
+        
+        # 0) Define local parameters for easy of reading
+        if not isscalar(self._step):
+            h = self._step[0]
+        else:
+            h = self._step
+        h_max = step_max
+        e = self.__eps
+
+        # 1) Compute parameter omega
+        w = h/( h + e )
+
+        # 2) Evaluate function at starting point X0
+        #    and compute ancillary variable z0
+        Z0  = h*fcn_hndl( X0 )
+
+        # 3) Compute new point x amd evaluate function
+        # NOTE: Xn1 = X_(n+1)
+        Xn1_0  = X0 - Z0
+        rho = norm( fcn_hndl( Xn1_0 ) )
+
+        # 4) Start the iterative computation
+        Zn    = Z0
+        Xn    = X0
+        rho_n = rho
+        h_n   = h
+        n     = 0
+
+        always_en = False
+        while ( n < self._max_iter ) and ( rho_n > self._tol ):
+            
+            Zn1 = w*( e*fcn_hndl( Xn1_0 ) + Zn )
+            Xn1 = Xn - Zn1
+
+            n     = n+1
+            Xn    = Xn1
+            Zn    = Zn1
+            Xn1_0 = Xn - Zn
+            rho_n1 = norm( fcn_hndl(Xn1_0) )
+
+            # Only for the implicit case?
+            # Limit the change up to 50%
+            sigma_n = log(rho_n1)-log(rho_n)
+            h_n1    = h_n
+            if sigma_n > -0.5 or always_en:
+                h_n1 = round( min( h_max , h_n*rho_n/rho_n1 , 1.5*h_n ) , 4 )
+            
+            h_n   = h_n1
+            rho_n = rho_n1
+
+            w = h_n/( h_n + e )
 
             if ( n >= self._max_iter ):
                 print("Solver stopped since max number of iterations reached")
